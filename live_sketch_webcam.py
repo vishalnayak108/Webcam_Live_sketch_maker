814a48e4f755987be483ce9a3beecb8a5401351d
vishal nayak <vishaln830@gmail.com>
2018-05-24 23:32:18 +0530

    First commit Message

---
+++ live_sketch_webcam.py
@@ -0,0 +1,24 @@
+import cv2
+import numpy as np
+
+def sketch(image):
+    img_gray= cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    img_gray_blur= cv2.GaussianBlur(img_gray, (5,5), 0)
+
+    canny_edge= cv2.Canny(img_gray_blur, 20, 20)
+
+    ret, mask= cv2.threshold(canny_edge, 60, 255, cv2.THRESH_BINARY_INV)
+
+    return mask
+
+cap= cv2.VideoCapture(0)
+
+while True:
+    ret, frame=cap.read()
+    cv2.imshow("Live sketcher", sketch(frame))
+    if cv2.waitKey(1)==13:
+       break
+
+cap.release()
+cv2.destroyAllWindows()
